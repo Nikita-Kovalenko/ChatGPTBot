dc66f0592efd9cf465716274cb4e6a17b4847dbf
Nikita-Kovalenko <89970243+Nikita-Kovalenko@users.noreply.github.com>
2023-04-21 22:14:27 +0300

    Update chat.py

--- ChatGPTBot/chat.py
+++ ChatGPTBot/chat.py
@@ -5,7 +5,7 @@ from aiogram.utils import executor
 
 token = '6129431252:AAH8J_oesmNgRKk3j0ZwW0tS4CNdmPlu_Bk'
 
-openai.api_key = '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'
+openai.api_key = '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'
 
 bot = Bot(token)
 dp = Dispatcher(bot)
